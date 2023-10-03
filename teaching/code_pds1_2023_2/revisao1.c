af3a3e81ee39ccd974d4de9d9fff31df23e36369
Pedro O.S. Vaz de Melo <pedroolmo@gmail.com>
2023-10-03 14:37:04 -0300

    Update revisao1.c

---
+++ teaching/code_pds1_2023_2/revisao1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void ordenaDois(float *end1, float *end2) {
+	
+	float f_end1 = *end1;
+	float f_end2 = *end2;
+	
+	if(f_end1 > f_end2) {
+		*end1 = f_end2;
+		*end2 = f_end1;
+	}
+}
+
+void ordenaDois_v2(float *end1, float *end2) {
+	
+	float aux; 
+	
+	if(*end1 > *end2) {
+		aux = *end1;
+		*end1 = *end2;
+		*end2 = aux;
+	}
+}
+
+void ordenaTres(float *e1, float *e2, float *e3) {
+	ordenaDois(e1, e2);
+	ordenaDois(e1, e3);
+	ordenaDois(e2, e3);
+}
+
+void imprimeSeqDiv(int qtd, int t, int n) {
+	int i, q , sobra=0;
+	if(qtd<=0 || t<=0|| n<=0)
+		return;
+	for(i=0;i<t;i++){
+		q= (qtd+sobra)/n;
+		sobra=(qtd+sobra)%n;
+		printf("%d: %d",i+1,q);
+	}
+}
+
+int pFrac(float x, int n) {
+	//x = 3.141592
+	//n = 3
+	//retorna o inteiro 141
+	float fracionaria = x-(int)x;
+	int casas_frac = (int)(fracionaria*pow(10,n));
+	return casas_frac;
+}
+
+void main() {
+	float qtd, t, n;
+	do {
+		scanf("%f %f %f", &qtd, &t, &n);
+	} while(parteFrac(qtd,6) || parteFrac(t,6) || parteFrac(n,6));
+	ordenaTres(&n, &t, &qtd);
+	imprimeSeqDiv(qtd, t, n);
+}
+	
+	
+	
+	
+	
+	
+	
