4efb4e207339c8029bbb0bd80397e28b3cc69256
Pedro O.S. Vaz de Melo <pedroolmo@gmail.com>
2023-11-27 18:42:29 -0300

    revisao3.c

--- teaching/code_pds1_2023_2/revisao3.c
+++ teaching/code_pds1_2023_2/revisao3.c
@@ -27,14 +27,63 @@ void ImpAst(int n) {
 void impTri(int i, int n){
 	if (i == n){
 		ImpAst(i);
+		
 	}
-	ImpAst(i);
-	impTri(i + 1, n);
-	ImpAst(i);
+	else {
+		ImpAst(i);
+		impTri(i + 1, n);
+		ImpAst(i);
+	}
 }
 
 
+void strInv(char *str) {
+	if(str[0] == '\0') {
+		return;
+	}
+	strInv(str+1);
+	printf("%c", str[0]);
+	
+}
+
+int somaVet(int v[], int n) {
+	if(n==0) {
+		return 0;
+	}
+	return v[n-1] + somaVet(v, n-1);
+	
+	
+}
+
+void freeMatrix(int **M, int n){
+	if(n == 0) {
+		free(M);
+	}
+	else {
+		free(M[n-1]);
+		freeMatrix(M, n-1);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 void main() {
+	
+	char s[] = "bola";
+	strInv(s);
+	
+	
 	int i, n;
 	scanf("%d %d", &i, &n);
 	impTri(i, n);
