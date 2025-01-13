3c117f9c88888366d46e01d55c01abe1ae197941
Pedro O.S. Vaz de Melo <pedroolmo@gmail.com>
2025-01-13 18:20:27 -0300

    recursividade

---
+++ teaching/code_pds1_2024_2/triangulo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void imprimeAsteriscos(int n) {
+	while(n > 0) {
+		printf("*");
+		n--;
+	}
+	printf("\n");
+}
+
+void imprimeTriangulo(int i, int h) {
+	
+	//caso trivial:
+	
+	
+	//passo recursivo:
+	
+}
+
+
+int main() {
+	
+	int lateral, altura;
+	scanf("%d %d", &lateral, &altura);
+	imprimeTriangulo(lateral, altura);
+	
+}
