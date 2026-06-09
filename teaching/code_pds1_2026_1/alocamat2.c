05102eb336cc512e90f8c16bc773ca70741aa464
Pedro O.S. Vaz de Melo <pedroolmo@gmail.com>
2026-06-09 14:38:18 -0300

    Update alocamat2.c

--- teaching/code_pds1_2026_1/alocamat2.c
+++ teaching/code_pds1_2026_1/alocamat2.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10000
+
+void alocaMatriz2(int ***M, int n, int m) {
+	*M = (int**)malloc(n*sizeof(int*));
+	for(i=0; i<n; i++) {
+		(*M)[i] = (int*)malloc(m*sizeof(int));
+	}
+	
+	for(i=0; i<n; i++)
+		for(j=0; j<m; j++) {
+			(*M)[i][j] = 0;
+		}
+	
+}
+
+int** alocaMatriz(int n, int m) {
+	int **M = (int**)malloc(n*sizeof(int*));
+	for(i=0; i<n; i++) {
+		M[i] = (int*)malloc(m*sizeof(int));
+	}
+	
+	for(i=0; i<n; i++)
+		for(j=0; j<m; j++) {
+			M[i][j] = 0;
+		}
+		
+	return M;
+	
+}
 
 
 void main() {
 	
-	/*int M[MAX][MAX];
-	
-	
-	
+
 	int n, m, i, j;
 	scanf("%d %d", &n, &m);
-	*/
+
+
 	
-	int n=MAX, m=MAX, i, j;
+	int **M = alocaMatriz(n, m);
 	
-	int **M;
+	alocaMatriz2(&M, n, m);
+	
+	/*
 	
 	M = (int**)malloc(n*sizeof(int*));
 	for(i=0; i<n; i++) {
@@ -28,11 +55,8 @@ void main() {
 			//*(*(M+i)+j) = 0;
 			M[i][j] = i+j;
 		}
+	*/
 	
-	
-	M[MAX-1][MAX-1] = 123;
-	printf("\n->%d", M[MAX-1][MAX-1]);
-	/*
 	
 	for(i=0; i<n; i++) {
 		for(j=0; j<m; j++) {
@@ -40,7 +64,7 @@ void main() {
 		}
 		printf("\n");
 	}
-	*/
+
 	
 	
 			
